18a3b497155669c239999188cb841c3866c53269
mahirx <60250437+mahirx@users.noreply.github.com>
2020-09-18 08:18:26 +0600

    Update eqution_crossmultiplication.cpp

--- C/Part_01/05_A_bit_of_Math/eqution_crossmultiplication.cpp
+++ C/Part_01/05_A_bit_of_Math/eqution_crossmultiplication.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char const *argv[])
 {
-	double a1, a2, b1, b2, c1, c2, X, Y;
+	double a1, a2, b1, b2, c1, c2, d, X, Y;
 	printf("Xa1 + Yb1 = c1\n");
 	printf("Xa2 + Yb2 = c2\n");
 
@@ -27,12 +27,25 @@ int main(int argc, char const *argv[])
 	scanf("%lf", &c2);
 
 
-	X = (c1*b2 - c2*b1) / (b2*a1 - b1*a2);
-	Y = (c2*a1 - c1*a2) / (b2*a1 - b1*a2);
+
+	d = (b2*a1 - b1*a2);
 
 
-	printf("X is %lf\n", X);
-	printf("Y is %lf\n", Y);
+
+	if ((int)d == 0)
+	{
+		printf("this is not valid\n");
+	}
+
+
+	else{
+		X = (c1*b2 - c2*b1) / d;
+		Y = (c2*a1 - c1*a2) / d;
+
+
+		printf("X is %lf\n", X);
+		printf("Y is %lf\n", Y);
+	}
 
 	return 0;
 }
